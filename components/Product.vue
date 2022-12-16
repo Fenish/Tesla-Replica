8c3a3f1d83be37556919201207fb33154c50c478
Fenish <sohretalhadev@gmail.com>
2022-12-16 16:08:47 +0300

    fix image load errors

--- components/Product.vue
+++ components/Product.vue
@@ -14,24 +14,13 @@ const props = defineProps({
     type: String,
   },
 });
-var BackgroundDesktop = "";
-var BackgroundMobile = "";
-if (process.client) {
-  BackgroundDesktop = new URL("../assets/img/" + props.Background + "-Desktop.jpg", import.meta.url).href;
-  BackgroundMobile = new URL("../assets/img/" + props.Background + "-Mobile.jpg", import.meta.url).href;
-}
-
-var isLink = "";
-if (props.Href) {
-  isLink = "underline";
-}
 </script>
 
 <template>
   <ClientOnly>
     <div :id="props.Title.replace(' ', '-')" class="snap-start relative h-screen">
-      <img :alt="props.Title" :src="BackgroundDesktop" draggable="false" class="md:block hidden object-cover w-screen h-full" />
-      <img :alt="props.Title" :src="BackgroundMobile" draggable="false" class="md:hidden block object-cover w-screen h-full" />
+      <img :alt="props.Title" :src="`/img/${props.Background}-Desktop.jpg`" draggable="false" class="md:block hidden object-cover w-screen h-full" />
+      <img :alt="props.Title" :src="`/img/${props.Background}-Mobile.jpg`" class="md:hidden block object-cover w-screen h-full" />
     </div>
   </ClientOnly>
 </template>
