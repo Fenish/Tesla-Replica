45da127a34903ee927141990f41592ae51bdfe35
Fenish <sohretalhadev@gmail.com>
2022-12-16 17:14:08 +0300

    change img to nuxt-img

--- components/Product.vue
+++ components/Product.vue
@@ -14,13 +14,20 @@ const props = defineProps({
     type: String,
   },
 });
+/*
+IMAGES WITH IMPORT
+MAYBE I WILL USE THIS LATER
+
+const DesktopImg = new URL(`../assets/img/${props.Background}-Desktop.jpg`, import.meta.url).href;
+const MobileImg = new URL(`../assets/img/${props.Background}-Mobile.jpg`, import.meta.url).href;
+*/
 </script>
 
 <template>
   <ClientOnly>
     <div :id="props.Title.replace(' ', '-')" class="snap-start relative h-screen">
-      <img :alt="props.Title" :src="`/img/${props.Background}-Desktop.jpg`" draggable="false" class="md:block hidden object-cover w-screen h-full" />
-      <img :alt="props.Title" :src="`/img/${props.Background}-Mobile.jpg`" class="md:hidden block object-cover w-screen h-full" />
+      <nuxt-img :alt="props.Title" :src="`/img/${props.Background}-Desktop.jpg`" draggable="false" class="md:block hidden object-cover w-screen h-full" format="webp" />
+      <nuxt-img :alt="props.Title" :src="`/img/${props.Background}-Mobile.jpg`" draggable="false" class="md:hidden block object-cover w-screen h-full" format="webp" />
     </div>
   </ClientOnly>
 </template>
