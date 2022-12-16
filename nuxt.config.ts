c356258bbe98caa450f16f024411fb4cb515864e
Fenish <sohretalhadev@gmail.com>
2022-12-16 17:13:58 +0300

    add nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        '@nuxt/image-edge',
     ],
     components: true,
     tailwindcss: {
